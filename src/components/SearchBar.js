e605e718bea9e2dadeb8eb9471ecd4909e2f077f
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-07 11:42:00 +0200

    Styled the search bar.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet, View, Text, TextInput } from 'react-native'
+import { Feather } from '@expo/vector-icons'; 
+
+const SearchBar = () => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput placeholder="Search" style={styles.inputStyle}/>
+        </View>
+    )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: 'hsl(0, 0%, 90%)',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        marginTop: 16,
+    }, 
+    inputStyle:{
+        flex: 1,
+        fontSize:18,
+    },
+    iconStyle:{
+        fontSize: 36,
+        alignSelf: 'center',
+        marginHorizontal: 15,
+    }
+})
