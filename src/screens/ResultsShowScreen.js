2c20530a945194b9adb4d283703e00cc394df306
Björn Bergquist <bjorn.bergquist@codic.se>
2021-10-14 14:08:50 +0200

    Added api call to screen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Yelp from '../api/Yelp'
 
 const ResultsShowScreen = ({navigation}) => {
+    const [result, setResult] = useState(null)
     const id = navigation.getParam('id')
 
-    console.log(id)
+    console.log(result);
+
+    const getResult = async (id) => {
+        const response = await Yelp.get(`/${id}`)
+        setResult(response.data)
+    }
+
+    useEffect(() => {
+        getResult(id)
+    }, [])
+
     return (
         <View>
             <Text>Results Show</Text>
